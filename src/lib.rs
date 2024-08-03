c58ef41622f5d42cc8346aaf26db8f51764f2b0c
Elias Gottsbacher <eli.gottsbacher@gmail.com>
2024-08-03 04:47:10 +0200

    better exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,19 @@
-pub mod buffer;
+mod buffer;
 mod light;
-pub mod model;
+mod model;
 pub mod picking;
 mod shared;
 pub mod texture;
 pub mod vertex;
 mod wrap;
+
+pub use buffer::Buffer;
+pub use buffer::IndexedBuffer;
+
+pub use buffer::alloc;
+
+pub use model::transform;
+
+pub use model::geometry::Geometry;
+
+pub use model::{BufferLocation, Expandable, TreeHandle, TreeModel};
--- src/picking/hitbox.rs
+++ src/picking/hitbox.rs
@@ -1,5 +1,3 @@
-use std::sync::{Arc, Mutex};
-
 use glam::Vec3;
 
 use crate::{
