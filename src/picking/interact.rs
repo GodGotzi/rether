13c9edd90b798f438b7ed53dce688201577a4f51
Elias Gottsbacher <eli.gottsbacher@gmail.com>
2024-08-30 16:18:41 +0200

    derive debug and clone on Interactive Events

--- src/picking/interact.rs
+++ src/picking/interact.rs
@@ -1,20 +1,24 @@
 use glam::Vec2;
 use winit::{event::MouseButton, keyboard::KeyCode};
 
+#[derive(Debug, Clone)]
 pub enum Action {
     Mouse(MouseButton),
     Keyboard(KeyCode),
 }
 
+#[derive(Debug, Clone)]
 pub struct DragEvent {
     pub delta: Vec2,
     pub action: Action,
 }
 
+#[derive(Debug, Clone)]
 pub struct ClickEvent {
     pub action: Action,
 }
 
+#[derive(Debug, Clone)]
 pub struct ScrollEvent {
     pub delta: f32,
     pub action: Action,
